f671f7eb946bac7686fe32c315d3143255fa6f32
Krittayapornn <krittayaporn.ww@gmail.com>
2024-05-09 15:44:38 +0700

    feat : boolean

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
+let isOver18 =true
+let hasCriminalBlacklist = true;
+let isAllow = true;
 
 // Start coding here
+let james = (!isOver18 && !hasCriminalBlacklist)
+console.log(james);
 
+isAllow = (!isOver18 && !hasCriminalBlacklist)
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,15 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = true;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = true;
+let hasPromotion = true;
 
-let hasPromotion;
+let john = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent && !isPlatinum)
+console.log(john);
 
-console.log(hasPromotion);
+hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent && !isPlatinum);
+console.log(hasPromotion)
